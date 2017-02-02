86196f6c2a167d27c57dd6e0682c39f8d6c8ead6
De Nayer <denayer.nicolas@gmail.com>
2017-02-02 09:40:45 +0100

    feat(data): from server

--- server.js
+++ server.js
@@ -1,2 +1,35 @@
 const express = require('express');
-express().use('/', express.static(__dirname)).listen(9000);
+const fs = require('fs');
+// const url = require('url');
+
+const app = express();
+
+const filePath = __dirname + '/data.json';
+
+app.post('/update', (request, respond) => {
+  let body = '';
+  request.on('data', data => {
+    body += data;
+  });
+  request.on('end', () => {
+    fs.appendFile(filePath, body, () => {
+      respond.end();
+    });
+  });
+});
+
+app.get('/load', (request, respond) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      respond.send({err});
+    }
+    try {
+      respond.send(JSON.parse(data));
+    } catch (exception) {
+      respond.send({exception});
+    }
+  });
+});
+app.use('/', express.static(__dirname));
+
+app.listen(9000);
--- src/components/EpicsList.js
+++ src/components/EpicsList.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react';
+import {Observable} from 'rxjs';
+import $ from 'jquery';
 
 class EpicsList extends Component {
   state = {epics: []};
+
+
+  componentWillMount() {
+    const requests$ = Observable.of('load');
+    const responses$ = requests$.flatMap(requestUrl => {
+      return Observable.fromPromise($.getJSON(requestUrl));
+    });
+
+    responses$.subscribe(response => {
+      this.setState({epics: response.epics});
+    });
+  }
+
+
   render() {
     return (
       <ul>
